1116a5ebccb459fc671b505a8a435d0788010da8
Lucas Lannes <lucaslannesr@gmail.com>
2025-06-14 01:19:56 -0300

    Add enemy animation

--- godot/scripts/enemy.gd
+++ godot/scripts/enemy.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@onready var animated_sprite = $AnimatedSprite2D
 # Pega uma referência ao nó filho
 @onready var thrower = $Thrower
 
@@ -14,3 +15,11 @@ extends Node2D
 func _ready():
 	# Garante que os valores iniciais sejam aplicados quando a cena carregar
 	thrower.target_node = target_node
+	thrower.throw.connect(_on_throw)
+
+func _on_throw():
+	animated_sprite.play("throw")
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite.animation == "throw":
+		animated_sprite.play("idle")
