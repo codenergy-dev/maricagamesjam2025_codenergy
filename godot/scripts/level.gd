56a1c5c81ff338f8186fde1e88fbc75a312a0dc7
Lucas Lannes <lucaslannesr@gmail.com>
2025-06-17 11:53:19 -0300

    Instantiate camera scene in level script

--- godot/scripts/level.gd
+++ godot/scripts/level.gd
@@ -5,8 +5,7 @@ func _ready():
 	await get_tree().process_frame
 	var game = get_tree().get_first_node_in_group("game")
 	if not game:
-		var camera = Camera2D.new()
-		camera.position = Vector2(0, -192)
-		camera.zoom = Vector2(0.75, 0.75)
-		add_child(camera)
+		var camera_scene = load("res://scenes/camera.tscn")
+		var camera_instance = camera_scene.instantiate()
+		add_child(camera_instance)
 		add_to_group("game")
