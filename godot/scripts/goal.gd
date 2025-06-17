ab1dbfe9dacb4f2bae716a6ba6eb216c1a96efb3
Lucas Lannes <lucaslannesr@gmail.com>
2025-06-17 15:38:46 -0300

    Call queue_free after goal is_colliding

--- godot/scripts/goal.gd
+++ godot/scripts/goal.gd
@@ -2,13 +2,9 @@ extends Node
 
 @onready var goal: RayCast2D = $RayCast2D
 
-var goal_reached = false
-
 func _physics_process(delta: float) -> void:
-	if not goal_reached and goal.is_colliding():
+	if goal.is_colliding():
 		var player = goal.get_collider()
-		goal_reached = true
-		
 		var game = get_tree().get_first_node_in_group("game")
 		var level = get_tree().get_nodes_in_group("level")[-1]
 		var next = level.find_child("Next")
@@ -16,3 +12,4 @@ func _physics_process(delta: float) -> void:
 		var next_level_instance = next_level_scene.instantiate()
 		next_level_instance.global_position = next.global_position
 		game.add_child(next_level_instance)
+		queue_free()
