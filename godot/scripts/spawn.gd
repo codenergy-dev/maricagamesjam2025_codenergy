69534d8ab105eef4a77616a3512bc35790fd981e
Lucas Lannes <lucaslannesr@gmail.com>
2025-06-17 00:50:28 -0300

    Export spawn_map from spawn script

--- godot/scripts/spawn.gd
+++ godot/scripts/spawn.gd
@@ -2,10 +2,7 @@ extends TileMapLayer
 
 # ...
 # Dicionário mapeia o texto do custom data para a cena
-var enemy_map = {
-	"bald": "res://scenes/enemy.tscn",
-	"bee": "res://scenes/bee.tscn"
-}
+@export var spawn_map: Dictionary[String, PackedScene] = {}
 
 func _ready():
 	await get_tree().process_frame
@@ -18,23 +15,22 @@ func spawn_enemies():
 	for cell_coords in used_cells:
 		var tile_data = get_cell_tile_data(cell_coords)
 		if tile_data:
-			var enemy = tile_data.get_custom_data("enemy")
-			if enemy_map.has(enemy):
+			var spawn = tile_data.get_custom_data("spawn")
+			if spawn_map.has(spawn):
 				# 1. Pega o caminho da cena do inimigo
-				var enemy_scene_path = enemy_map[enemy]
-				var enemy_scene = load(enemy_scene_path)
+				var spawn_scene = spawn_map[spawn]
 
-				if enemy_scene:
+				if spawn_scene:
 					# 2. Calcula a posição no mundo para o spawn
 					var spawn_position = to_global(map_to_local(cell_coords))
 
 					# Instancia a cena do inimigo
-					var enemy_instance = enemy_scene.instantiate()
-					enemy_instance.global_position = spawn_position
+					var spawn_instance = spawn_scene.instantiate()
+					spawn_instance.global_position = spawn_position
 
 					# Adiciona o inimigo à cena (como irmão do TileMap, por exemplo)
 					var game = get_tree().get_first_node_in_group("game")
-					game.add_child(enemy_instance)
+					game.add_child(spawn_instance)
 
 					# 3. Apaga o tile marcador para que ele não apareça no jogo
 					set_cell(cell_coords, -1)
