33ef1fc28721396863acf1d67a1ef86cb603e5db
Lucas Lannes <lucaslannesr@gmail.com>
2025-06-17 15:32:47 -0300

    Reset level when player not exists

--- godot/scripts/level.gd
+++ godot/scripts/level.gd
@@ -1,5 +1,7 @@
 extends Node
 
+static var current_level: Node
+
 func _ready():
 	add_to_group("level")
 	await get_tree().process_frame
@@ -9,3 +11,18 @@ func _ready():
 		var camera_instance = camera_scene.instantiate()
 		add_child(camera_instance)
 		add_to_group("game")
+
+func _physics_process(delta: float) -> void:
+	if current_level != self:
+		return
+	var player = get_tree().get_first_node_in_group("player")
+	if not player:
+		var game = get_tree().get_first_node_in_group("game")
+		var children = game.get_children()
+		for child in children:
+			if child.name != "Camera":
+				child.queue_free()
+		
+		var level_scene = load(scene_file_path)
+		var level_instance = level_scene.instantiate()
+		game.add_child(level_instance)
