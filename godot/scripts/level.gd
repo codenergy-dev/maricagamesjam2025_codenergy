c57c252c0d0a4c315e7851ede9c9700c4961f870
Lucas Lannes <lucaslannesr@gmail.com>
2025-06-17 01:30:53 -0300

    Add level script to configure camera if it not exists

---
+++ godot/scripts/level.gd
@@ -0,0 +1,11 @@
+extends Node
+
+func _ready():
+	await get_tree().process_frame
+	var game = get_tree().get_first_node_in_group("game")
+	if not game:
+		var camera = Camera2D.new()
+		camera.position = Vector2(0, -192)
+		camera.zoom = Vector2(0.75, 0.75)
+		add_child(camera)
+		add_to_group("game")
