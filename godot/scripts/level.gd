55409d0f4e19f2b64f39db8546a39c8b63d8d4ab
Lucas Lannes <lucaslannesr@gmail.com>
2025-06-17 16:27:36 -0300

    Add camera group

--- godot/scripts/level.gd
+++ godot/scripts/level.gd
@@ -7,10 +7,12 @@ func _ready():
 	await get_tree().process_frame
 	var game = get_tree().get_first_node_in_group("game")
 	if not game:
+		add_to_group("game")
+	var camera = get_tree().get_first_node_in_group("camera")
+	if not camera:
 		var camera_scene = load("res://scenes/camera.tscn")
 		var camera_instance = camera_scene.instantiate()
 		add_child(camera_instance)
-		add_to_group("game")
 
 func _physics_process(delta: float) -> void:
 	if current_level != self:
