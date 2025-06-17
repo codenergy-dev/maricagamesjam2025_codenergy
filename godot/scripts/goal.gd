bedebd2ef163c9eefe6b4fa27ef5e665546a4d2e
Lucas Lannes <lucaslannesr@gmail.com>
2025-06-17 17:32:45 -0300

    Find next level in goal script

--- godot/scripts/goal.gd
+++ godot/scripts/goal.gd
@@ -8,8 +8,9 @@ func _physics_process(delta: float) -> void:
 		var game = get_tree().get_first_node_in_group("game")
 		var level = get_tree().get_nodes_in_group("level")[-1]
 		var next = level.find_child("Next")
-		var next_level_scene = load(level.scene_file_path)
-		var next_level_instance = next_level_scene.instantiate()
-		next_level_instance.global_position = next.global_position
-		game.add_child(next_level_instance)
+		var next_level_scene = game.next_level() if game.name == "Game" else load(level.scene_file_path)
+		if next_level_scene:
+			var next_level_instance = next_level_scene.instantiate()
+			next_level_instance.global_position = next.global_position
+			game.add_child(next_level_instance)
 		queue_free()
