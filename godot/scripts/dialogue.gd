5d3fdf555b8d04462e55ae461b911329ef4fdc98
Lucas Lannes <lucaslannesr@gmail.com>
2025-06-27 18:16:18 -0300

    Update player state and controls during dialogue

--- godot/scripts/dialogue.gd
+++ godot/scripts/dialogue.gd
@@ -10,4 +10,14 @@ var started = false
 func _physics_process(delta: float) -> void:
 	if not started and raycast.is_colliding():
 		started = true
+		
+		var player: Player = DialogueManager.get_current_scene.call().player
+		var player_previous_state = player.state_name
+		player.change_state("dialogue")
+		player.controls.hide()
+		
 		DialogueManager.show_dialogue_balloon(resource, start)
+		await DialogueManager.dialogue_ended
+		
+		player.change_state(player_previous_state)
+		player.controls.show()
