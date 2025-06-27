b9b92826b2026c79294a985473cda2fff42449f1
Lucas Lannes <lucaslannesr@gmail.com>
2025-06-27 13:46:25 -0300

    Set camera limits based on current level

--- godot/scripts/goal.gd
+++ godot/scripts/goal.gd
@@ -4,10 +4,4 @@ extends Node
 
 func _physics_process(delta: float) -> void:
 	if goal.is_colliding():
-		var player = goal.get_collider()
-		var game = get_tree().get_first_node_in_group("game")
-		var next = game.find_children("Next", "", true, false)[-1]
-		var next_level = Game.next_level()
-		next_level.global_position = next.global_position
-		game.add_child(next_level)
 		queue_free()
--- godot/scripts/level.gd
+++ godot/scripts/level.gd
@@ -1,9 +1,11 @@
 class_name Level
-extends Node
+extends Node2D
 
 @export var player_sprite_frames: String = "default"
 
 var index: int = 0
+
+@onready var next = $Next
 
 func _ready():
 	add_to_group("level")
@@ -11,11 +13,12 @@ func _ready():
 	var game = get_tree().get_first_node_in_group("game")
 	if not game:
 		add_to_group("game")
-	var camera = get_tree().get_first_node_in_group("camera")
+	var camera: Camera2D = get_tree().get_first_node_in_group("camera")
 	if not camera:
 		var camera_scene = load("res://scenes/camera.tscn")
-		var camera_instance = camera_scene.instantiate()
-		add_child(camera_instance)
+		camera = camera_scene.instantiate()
+		add_child(camera)
+	camera.limit_right = next.global_position.x
 	await get_tree().process_frame
 	var player = get_tree().get_first_node_in_group("player")
 	if player:
@@ -25,6 +28,21 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	if Game.current_level != self:
 		return
+	
+	var camera: Camera2D = get_tree().get_first_node_in_group("camera")
+	var camera_center = camera.get_screen_center_position()
+	var camera_transform = camera.get_canvas_transform().affine_inverse()
+	var camera_left = camera_transform.origin.x
+	var camera_right = camera_center.x + (camera_center.x - camera_left)
+	if camera_left >= global_position.x:
+		camera.limit_left = global_position.x
+	if camera_right >= next.global_position.x and Game.auto_level_index == index:
+		var game = get_tree().get_first_node_in_group("game")
+		var next_level = Game.next_level()
+		camera.limit_right = 10000000
+		next_level.global_position = next.global_position
+		game.add_child(next_level)
+	
 	var player = get_tree().get_first_node_in_group("player")
 	if not player:
 		Game.reset_level(self)
