f1503138274e4ffc56dc2645ed92821fe44992ac
Lucas Lannes <lucaslannesr@gmail.com>
2025-06-30 11:32:54 -0300

    Set jellyfish collision shape dynamically

--- godot/scripts/jellyfish.gd
+++ godot/scripts/jellyfish.gd
@@ -1,12 +1,15 @@
 extends Node
 
 @onready var sprite = $Sprite2D
-@onready var collision_shape = $CollisionShape2D.shape as CapsuleShape2D
+@onready var collision_shape = $CollisionShape2D
 @onready var audio = $AudioManager
 
-func _physics_process(delta: float) -> void:
-	collision_shape.radius = sprite.region_rect.size.x * 0.5
-	collision_shape.height = sprite.region_rect.size.y - (collision_shape.radius * 2)
+func _ready() -> void:
+	await get_tree().process_frame
+	var capsule_shape = CapsuleShape2D.new()
+	capsule_shape.radius = sprite.region_rect.size.x * 0.25
+	capsule_shape.height = sprite.region_rect.size.y
+	collision_shape.shape = capsule_shape
 
 func _on_area_entered(area: Area2D) -> void:
 	audio.play("hit")
