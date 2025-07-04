d05f34758bd5193b385c7a483b067836673f916c
Lucas Lannes <lucaslannesr@gmail.com>
2025-07-04 17:25:09 -0300

    Add transition to load game

--- godot/scripts/transition.gd
+++ godot/scripts/transition.gd
@@ -11,5 +11,9 @@ func _ready() -> void:
 	queue_free()
 
 func on_transition():
+	if type == "load_game":
+		Game.clear_level(Game.current_level)
+		Game.reset_game()
+		get_tree().change_scene_to_file("res://scenes/game.tscn")
 	if type == "reset_level":
 		Game.reset_level(Game.current_level)
