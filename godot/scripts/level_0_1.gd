98a456f86a45cbf21ba4aa14030f9d8a21150140
Lucas Lannes <lucaslannesr@gmail.com>
2025-07-04 17:41:30 -0300

    Add level 0 1 script

---
+++ godot/scripts/level_0_1.gd
@@ -0,0 +1,11 @@
+extends Level
+
+@onready var audio: AudioManager = $AudioManager
+
+func arcade_abducts_player():
+	var arcade: Arcade = get_tree().get_first_node_in_group("game").find_child("Arcade", true, false)
+	audio.play("abduction")
+	arcade.animation_player.play("abducts_player")
+	player.animation_player.play("abducted")
+	await arcade.animation_player.animation_finished
+	TransitionManager.transition("load_game")
