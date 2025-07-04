b2296c6d58b2105fce0a9203ef80225f40bc7411
Lucas Lannes <lucaslannesr@gmail.com>
2025-07-04 17:26:20 -0300

    Add arcade script

---
+++ godot/scripts/arcade.gd
@@ -0,0 +1,10 @@
+class_name Arcade
+extends CharacterBody2D
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += ProjectSettings.get_setting("physics/2d/default_gravity") * delta
+	move_and_slide()
