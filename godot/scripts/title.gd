0dfb568df1827ccd30771e33220c163b5fdb5fa9
Lucas Lannes <lucaslannesr@gmail.com>
2025-07-05 18:34:58 -0300

    Use audio fade function in title script

--- godot/scripts/title.gd
+++ godot/scripts/title.gd
@@ -8,10 +8,5 @@ func _ready() -> void:
 
 func _on_button_pressed() -> void:
 	audio.play("select")
-	await fade_out_music()
+	await AudioFade.out(music)
 	TransitionManager.transition("start_game")
-
-func fade_out_music(duration := 1.0) -> Signal:
-	var tween = create_tween()
-	tween.tween_property(music, "volume_db", -80, duration)
-	return tween.finished
