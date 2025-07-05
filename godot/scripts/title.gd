d74cfa5c5f6559a6fb6e39420e934a696c61d548
Lucas Lannes <lucaslannesr@gmail.com>
2025-07-05 18:40:42 -0300

    Start game on any key pressed in title script

--- godot/scripts/title.gd
+++ godot/scripts/title.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var has_started_game = false
+
 @onready var music = $AudioStreamPlayer
 @onready var audio = $AudioManager
 
@@ -7,6 +9,16 @@ func _ready() -> void:
 	get_tree().root.add_to_group("game")
 
 func _on_button_pressed() -> void:
+	start_game()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed and not event.echo:
+		start_game()
+
+func start_game() -> void:
+	if has_started_game:
+		return
+	has_started_game = true
 	audio.play("select")
 	await AudioFade.out(music)
 	TransitionManager.transition("start_game")
