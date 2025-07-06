cd6854bf04e66729b52258164dc3ae7417fb0294
Lucas Lannes <lucaslannesr@gmail.com>
2025-07-06 17:58:51 -0300

    Add transition to level select

--- godot/scripts/transition.gd
+++ godot/scripts/transition.gd
@@ -18,5 +18,19 @@ func on_transition():
 		Game.clear_level(Game.current_level)
 		Game.reset_game()
 		get_tree().change_scene_to_file("res://scenes/game.tscn")
+	elif type.begins_with("level_select_"):
+		var level = int(type.split("_")[2]) if type.split("_").size() == 3 else 0
+		var level_select = get_tree().root.get_node("LevelSelect")
+		
+		if is_instance_valid(level_select):
+			level_select.queue_free()
+		elif is_instance_valid(Game.current_level):
+			Game.clear_level(Game.current_level)
+			Game.reset_game()
+		
+		if level == 0:
+			get_tree().change_scene_to_file("res://scenes/levels/level_0_1.tscn")
+		else:
+			get_tree().change_scene_to_file("res://scenes/game.tscn")
 	elif type == "reset_level":
 		Game.reset_level(Game.current_level)
