722ec6546951ce64a57d7af8b28589248f4ae19f
Lucas Lannes <lucaslannesr@gmail.com>
2025-07-06 18:29:49 -0300

    Update transition to start game

--- godot/scripts/transition.gd
+++ godot/scripts/transition.gd
@@ -13,7 +13,7 @@ func _ready() -> void:
 func on_transition():
 	if type == "start_game":
 		get_tree().root.get_node("Title").queue_free()
-		get_tree().change_scene_to_file("res://scenes/levels/level_0_1.tscn")
+		get_tree().change_scene_to_file("res://scenes/level_select.tscn")
 	elif type == "load_game":
 		Game.clear_level(Game.current_level)
 		Game.reset_game()
