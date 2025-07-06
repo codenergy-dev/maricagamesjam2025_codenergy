5c060b810864422fe1e288ba16181ba3a870c55a
Lucas Lannes <lucaslannesr@gmail.com>
2025-07-06 17:49:44 -0300

    Ignore player controls on dialogue

--- godot/scripts/dialogue.gd
+++ godot/scripts/dialogue.gd
@@ -14,10 +14,8 @@ func _physics_process(delta: float) -> void:
 		var player: Player = DialogueManager.get_current_scene.call().player
 		var player_previous_state = player.state_name
 		player.change_state("dialogue")
-		player.controls.hide()
 		
 		DialogueManager.show_dialogue_balloon(resource, start)
 		await DialogueManager.dialogue_ended
 		
 		player.change_state(player_previous_state)
-		player.controls.show()
