43f83597bd5d067fb746c10a76820816fbc2c2a5
Lucas Lannes <lucaslannesr@gmail.com>
2025-07-07 23:28:09 -0300

    Remove alien laser on knockback

--- godot/scripts/alien.gd
+++ godot/scripts/alien.gd
@@ -2,9 +2,11 @@ extends CharacterBody2D
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var hitbox: Area2D = $Hitbox
+@onready var laser: Laser = $Laser
 
 func _on_knockback_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
+		laser.queue_free()
 		hitbox.queue_free()
 		animated_sprite.play("knockback")
 		await animated_sprite.animation_finished
