0421add3618d350c96df788e69f5b8cd5cc9eb47
Lucas Lannes <lucaslannesr@gmail.com>
2025-07-07 23:30:41 -0300

    Allow alien knockback only if visible on screen

--- godot/scripts/alien.gd
+++ godot/scripts/alien.gd
@@ -3,8 +3,11 @@ extends CharacterBody2D
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var hitbox: Area2D = $Hitbox
 @onready var laser: Laser = $Laser
+@onready var visible_on_screen: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
 
 func _on_knockback_area_entered(area: Area2D) -> void:
+	if not visible_on_screen.is_on_screen():
+		return
 	if area.is_in_group("player"):
 		laser.queue_free()
 		hitbox.queue_free()
