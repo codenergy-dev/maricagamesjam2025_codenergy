18e07dacb80d31c62ee17ed31eeada6d32353292
Lucas Lannes <lucaslannesr@gmail.com>
2025-07-08 18:39:52 -0300

    Add level 6 1 script

---
+++ godot/scripts/level_6_1.gd
@@ -0,0 +1,17 @@
+extends Level
+
+func activate_arcade():
+	var arcade: Arcade = get_tree().get_first_node_in_group("game").find_child("Arcade", true, false)
+	arcade.laser.enable_auto_shoot()
+	
+	var audio_stream_player = AudioStreamPlayer.new()
+	audio_stream_player.name = "Charge"
+	audio_stream_player.stream = load("res://assets/sounds/laser_charge.wav")
+	arcade.laser.audio.add_child(audio_stream_player)
+	arcade.laser.audio.sounds["charge"] = audio_stream_player
+	arcade.laser.animation_player.connect("animation_started", on_arcade_animation_started)
+
+func on_arcade_animation_started(animation: String):
+	if animation == "charge":
+		var arcade: Arcade = get_tree().get_first_node_in_group("game").find_child("Arcade", true, false)
+		arcade.laser.audio.play("charge")
